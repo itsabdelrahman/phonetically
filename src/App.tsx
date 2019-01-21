9581f29be5eea463bc676d9e9a96db889a4d8c6e
A.R. Maged <abdulrahman.maged95@gmail.com>
2019-01-21 15:55:18 +0100

    :sparkles: Build page with text input

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,48 @@
 import * as React from 'react';
-import './App.css';
+import { Grommet, Box, TextInput, ThemeValue } from 'grommet';
 
-import logo from './logo.svg';
+const theme: ThemeValue = {
+  global: {
+    font: {
+      family: 'Lato',
+      size: '40px',
+      height: '20px',
+    },
+  },
+};
 
-class App extends React.Component {
+type Props = {};
+
+type State = {
+  text: string;
+};
+
+class App extends React.Component<Props, State> {
+  public state: State = {
+    text: '',
+  };
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </div>
+      <Grommet theme={theme} full={true}>
+        <Box
+          fill={true}
+          flex={true}
+          pad="large"
+          background={{ color: 'black' }}
+          border={{ color: 'accent-3', size: 'large' }}
+        >
+          <TextInput
+            plain={true}
+            placeholder="Type something here"
+            autoFocus={true}
+            value={this.state.text}
+            onChange={(event: any) => {
+              this.setState({ text: event.target.value });
+            }}
+          />
+        </Box>
+      </Grommet>
     );
   }
 }
